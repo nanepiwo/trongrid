52e73f1aa86e8bc9d1b683b5f723ad5a61a10997
nanepiwo <spamdumpingstation@gmail.com>
2015-02-19 12:32:09 -0500

    to suit my server

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # TronGrid
 
+Nanepiwo's fork of the Gabo's tron[grid] mod: 
+
 Yet another Tron-inspired mod.
 * Author: GaboXandre
-* Tron Tool textures: [Jericho](https://github.com/JerichoEdwards)
 
 *Special thanks to [Amaz1](https://github.com/Amaz1) and [foot-on-teh-hill](https://github.com/foot-on-teh-hill) for encouragement, advise and inspiration. Also to [nanepiwo](https://github.com/nanepiwo) for his million ideas.*
 
@@ -13,22 +14,12 @@ All blocks available in blue, green and red.
 Glowing blocks:
 * Center/corner blocks
 * Horizontal lines
-* Vertical lines
+* Vertical lines/Pillars
 * Tron Glass
 * Tron Elevator blocks (~ ladders)
 
 Non-glowing blocks:
 * Tron black grid block
-
-Tron Tools:
-* Tron Sword
-* Tron Shovel
-* Tron Pick
-* Tron Axe
-
-Special blocks (required for recipes):
-* Tron Crystals
-* Tron Sticks
 
 ## Dependencies
 * default
@@ -37,15 +28,9 @@ Special blocks (required for recipes):
 
 ## License
 
-For both textures and code by GaboXandre: [WTFPL](http://www.wtfpl.net/)
-
+For both textures and code by GaboXandre: [WTFPL](http://www.wtfpl.net/
+More code, though little: nanepiwo [WTFPL]
 
 ## Note
 This is my first mod. Code is certainly ugly, if not downright hideous. 
 All criticsm is welcome... as long as it is constructive.
-
-
-
-
-
-
--- trongrid/init.lua
+++ trongrid/init.lua
@@ -1,150 +1,124 @@
 -- Register tronblocks
-minetest.register_craftitem("trongrid:tron_crystal", {
-	description = "Tron Crystal",
-	inventory_image = "trongrid_troncrystal.png",
-})
 
-minetest.register_craftitem("trongrid:tron_stick", {
-	description = "Tron Stick",
-	inventory_image = "trongrid_tron_stick.png",
-})
+-- Coloured
 
-minetest.register_node("trongrid:tronblock_center" , {
-	description = "Tronblock Center Stone",
-	drawtype = "glasslike",
-	paramtype = "light",
-	light_source = 15,
-	is_ground_content = false,
-	sunlight_propagates = true,
-	iventory_image = "trongrid_tronblock_center.png",
-	tiles = {"trongrid_tronblock_center.png"},
-	groups = {cracky = 3},
-})
+local colours = {"blue","red","green"}
+local colours2 = {"Blue","Red","Green"}
 
-minetest.register_node("trongrid:tronblock_horizontal" , {
-	description = "Tronblock horizontal Stone",
-	drawtype = "glasslike",
-	paramtype = "light",
-	paramtype2 = "facedir",
-	light_source = 10,
-	is_ground_content = false,
-	sunlight_propagates = true,
-	iventory_image = "trongrid_tronblock_horizontal.png",
-	tiles = {
-		"trongrid_tronblock_horizontal.png",
-		"trongrid_tronblock_horizontal.png",
-		"trongrid_tronblock_horizontal.png",
-		"trongrid_tronblock_horizontal.png",
-		"trongrid_tronblock_horizontal.png",
-		"trongrid_tronblock_horizontal.png",
-	},
-	groups = {cracky = 3},
-})
+for number = 1,3 do
+
+	local lcolour = colours[number]
+	local ccolour = colours2[number]
+
+-- Blocks
+
+	minetest.register_node("trongrid:tronblock_center_"..lcolour , {
+		description = ccolour.." Tronblock Square Stone",
+		drawtype = "glasslike",
+		paramtype = "light",
+		light_source = 15,
+		is_ground_content = false,
+		sunlight_propagates = true,
+		iventory_image = "trongrid_tronblock_center_"..lcolour..".png",
+		tiles = {"trongrid_tronblock_center"..lcolour..".png"},
+		groups = {cracky = 3, oddly_breakable_by_hand = 1, stone = 1},
+	})
+
+	minetest.register_node("trongrid:tronblock_horizontal_"..lcolour , {
+		description = ccolour.." Tronblock Line Stone",
+		drawtype = "glasslike",
+		paramtype = "light",
+		paramtype2 = "facedir",
+		light_source = 10,
+		is_ground_content = false,
+		sunlight_propagates = true,
+		iventory_image = "trongrid_tronblock_horizontal_"..lcolour..".png",
+		tiles = {"trongrid_tronblock_horizontal_"..lcolour..".png"},
+		groups = {cracky = 3, oddly_breakable_by_hand = 1, stone = 1},
+	})
 
 
-minetest.register_node("trongrid:tronblock_vertical" , {
-	description = "Tronblock vertical Stone",
-	drawtype = "glasslike",
-	paramtype = "light",
-	light_source = 10,
-	is_ground_content = false,
-	sunlight_propagates = true,
-	iventory_image = "trongrid_tronblock_vertical.png",
-	tiles = {"trongrid_tronblock_vertical.png"},
-	groups = {cracky = 3},
-})
+	minetest.register_node("trongrid:tronblock_vertical_"..lcolour , {
+		description = ccolour.." Tronblock Pillar Stone",
+		drawtype = "glasslike",
+		paramtype = "light",
+		light_source = 10,
+		is_ground_content = false,
+		sunlight_propagates = true,
+		iventory_image = "trongrid_tronblock_vertical_"..lcolour..".png",
+		tiles = {
+			"trongrid_tronblock_center_"..lcolour..".png",
+			"trongrid_tronblock_center_"..lcolour..".png",
+			"trongrid_tronblock_vertical_"..lcolour..".png",
+			"trongrid_tronblock_vertical_"..lcolour..".png",
+			"trongrid_tronblock_vertical_"..lcolour..".png",
+			"trongrid_tronblock_vertical_"..lcolour..".png"
+		},
+		groups = {cracky = 3, oddly_breakable_by_hand = 1, stone = 1},
+	})
 
-minetest.register_node("trongrid:tronblock_black" , {
-	description = "Tronblock black Stone",
-	drawtype = "glasslike",
-	paramtype = "light",
-	light_source = 0,
-	is_ground_content = false,
-	sunlight_propagates = true,
-	iventory_image = "trongrid_tronblock_black.png",
-	tiles = {"trongrid_tronblock_black.png"},
-	groups = {cracky = 3},
-})
+	minetest.register_node("trongrid:tronblock_glass"..lcolour , {
+		description = ccolour.." Tronblock Glass Stone",
+		drawtype = "glasslike",
+		paramtype = "light",
+		light_source = 15,
+		is_ground_content = false,
+		sunlight_propagates = true,
+		iventory_image = "trongrid_tronblock_transparent_"..lcolour..".png",
+		tiles = {"trongrid_tronblock_transparent_"..lccolour..".png"},
+		groups = {cracky = 3, oddly_breakable_by_hand = 1, stone = 1},
+	})
 
-minetest.register_node("trongrid:tronblock_glass" , {
-	description = "Tronblock Glass Stone",
-	drawtype = "glasslike",
-	paramtype = "light",
-	light_source = 15,
-	is_ground_content = false,
-	sunlight_propagates = true,
-	iventory_image = "trongrid_tronblock_transparent.png",
-	tiles = {"trongrid_tronblock_transparent.png"},
-	groups = {fleshy = 3},
-})
+-- Crafts
+	
+	minetest.register_craft({
+		output = "trongrid:tronblock_horizontal_"..lcolour,
+		recipe = {{"dye:"..lcolour, "trongrid:black", "trongrid:black"}}
+	})
 
--- Register recipes
+	minetest.register_craft({
+		output = "trongrid:tronblock_vertical_"..lcolour,
+		recipe = {
+			 {"dye:"..lcolour},
+			 {"trongrid:black"},
+			 {"trongrid:black"}
+		}
+	})
 
-minetest.register_craft({
-	output = "trongrid:tron_crystal 4",
-	recipe = {
-		{"", "default:torch",""},
-		{"", "dye:blue",""},
-		{"", "default:coal_lump",""},
-	}
-})
+	minetest.register_craft({
+		output = "trongrid:tronblock_center_"..lcolour,
+		recipe = {
+			 {"trongrid:black", "trongrid:black", "trongrid:black"},
+			 {"trongrid:black", "dye:"..lcolour, "trongrid:black"},
+			 {"trongrid:black", "trongrid:black", "trongrid:black"},
+		}
+	})
+	
+end
 
-minetest.register_craft({
-	output = "trongrid:tron_stick 9",
-	recipe = {
-		{"", "",""},
-		{"", "trongrid:tron_crystal",""},
-		{"", "",""},
-	}
-})
+-- Black
 
-minetest.register_craft({
-	output = "trongrid:tronblock_center 9",
-	recipe = {
-		{"trongrid:tron_crystal", "trongrid:tron_crystal","trongrid:tron_crystal"},
-		{"trongrid:tron_crystal", "moreblocks:circle_stone_bricks", "trongrid:tron_crystal"},
-		{"trongrid:tron_crystal", "trongrid:tron_crystal","trongrid:tron_crystal"},
-	}
-})
 
-minetest.register_craft({
-	output = "trongrid:tronblock_horizontal 3",
-	recipe = {
-		{"", "",""},
-		{"trongrid:tron_crystal", "trongrid:tron_crystal", "trongrid:tron_crystal"},
-		{"", "",""},
-	}
-})
+	minetest.register_node("trongrid:black" , {
+		description = "Tronblock Black Stone",
+		drawtype = "glasslike",
+		paramtype = "light",
+		light_source = 0,
+		is_ground_content = false,
+		sunlight_propagates = true,
+		iventory_image = "trongrid_tronblock_black.png",
+		tiles = {"trongrid_tronblock_black.png"},
+		groups = {cracky = 3, oddly_breakable_by_hand = 1, stone = 1},
+	})
 
-minetest.register_craft({
-	output = "trongrid:tronblock_vertical 3",
-	recipe = {
-		{"", "trongrid:tron_crystal",""},
-		{"", "trongrid:tron_crystal", ""},
-		{"", "trongrid:tron_crystal",""},
-	}
-})
 
-minetest.register_craft({
-	output = "trongrid:tronblock_glass 3",
-	recipe = {
-		{"", "trongrid:tron_crystal",""},
-		{"", "default:glass", ""},
-		{"", "trongrid:tron_crystal",""},
-	}
-})
+	minetest.register_craft({
+		output = "trongrid:black 8",
+		recipe = {
+			{"dye:black"},
+			{"group:stone"},
+			{""},
+		}
+	})
 
-minetest.register_craft({
-	output = "trongrid:tronblock_black 3",
-	recipe = {
-		{"", "dye:black",""},
-		{"", "trongrid:tron_crystal", ""},
-		{"", "",""},
-	}
-})
-
-dofile(minetest.get_modpath("trongrid").."/red.lua")
-dofile(minetest.get_modpath("trongrid").."/green.lua")
-dofile(minetest.get_modpath("trongrid").."/tools.lua")
-dofile(minetest.get_modpath("trongrid").."/aliases.lua")
 dofile(minetest.get_modpath("trongrid").."/elevator.lua")
